3fe69b3b53a9f638a76fcb9da5fa292f50297671
Xinglingsuiyue <2321600440@qq.com>
2026-05-17 22:29:24 +0800

    Add files via upload

--- resources/Shanghai/enemies/无妄者.js
+++ resources/Shanghai/enemies/无妄者.js
@@ -1,1 +1,12 @@
+export default {
+  name: '无妄者',
 
+  build({ options = {} } = {}) {
+    return {
+      name: '无妄者',
+      level: Number(options.enemyLevel ?? 90),
+      resistance: Number(options.resistance ?? 0.1),
+      ignoreDefense: Number(options.ignoreDefense ?? 0)
+    }
+  }
+}
