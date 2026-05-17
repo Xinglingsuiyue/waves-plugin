7e0a864dd3d67c97f5e0d473d79dd180f342433c
Xinglingsuiyue <2321600440@qq.com>
2026-05-17 22:26:50 +0800

    Add files via upload

--- resources/Shanghai/phantoms/角.js
+++ resources/Shanghai/phantoms/角.js
@@ -1,1 +1,17 @@
+export default {
+  name: '角',
 
+  apply({ skillType }) {
+    // 常驻不算，只算触发后的“光阴之祝”
+    // 光阴之祝：共鸣技能伤害加成提升16%
+    if (skillType === 'skill' || skillType === '共鸣技能') {
+      return {
+        damageBonus: 0.16
+      }
+    }
+
+    return {
+      damageBonus: 0
+    }
+  }
+}
