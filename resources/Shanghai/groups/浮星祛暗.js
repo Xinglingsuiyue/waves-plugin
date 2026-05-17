553d3859aaead60b11c077bfa15f27c0cb80c34e
Xinglingsuiyue <2321600440@qq.com>
2026-05-17 22:25:27 +0800

    Add files via upload

--- resources/Shanghai/groups/浮星祛暗.js
+++ resources/Shanghai/groups/浮星祛暗.js
@@ -1,1 +1,19 @@
+export default {
+  name: '浮星祛暗',
 
+  apply({ equipment }) {
+    const count = Number(equipment?.groupCount || 0)
+
+    // 2件套常驻不重复计算
+    // 只计算5件套触发态
+    if (count >= 5) {
+      return {
+        damageBonus: 0.30
+      }
+    }
+
+    return {
+      damageBonus: 0
+    }
+  }
+}
